f6732e29f953ff601382eeaf223ab193cb7376d8
jsmayo <Steven@192.168.0.9>
2017-03-04 01:19:15 -0500

    Changing Math.. really shouldnt be this complicated?

--- Project1/src/edu/ncsu/csc216/checkout_simulator/queues/CheckoutRegister.java
+++ Project1/src/edu/ncsu/csc216/checkout_simulator/queues/CheckoutRegister.java
@@ -85,9 +85,9 @@ public class CheckoutRegister implements LineOfItems {
 			this.timeWhenAvailable = cart.getProcessTime() + cart.getArrivalTime();
 		}
 		else { 
-			this.timeWhenAvailable = this.timeWhenAvailable - cart.getArrivalTime();
-			if(this.timeWhenAvailable < cart.getArrivalTime()) cart.setWaitTime(0);
-			this.timeWhenAvailable += cart.getWaitTime() + cart.getProcessTime();
+			if (this.timeWhenAvailable < cart.getArrivalTime()) cart.setWaitTime(0);
+			else cart.setWaitTime(this.timeWhenAvailable - cart.getArrivalTime());
+			this.timeWhenAvailable += (cart.getWaitTime() + cart.getProcessTime());
 		}
 			
 		//if(this.timeWhenAvailable < cart.getArrivalTime() && !line.isEmpty()) cart.setWaitTime(0);
